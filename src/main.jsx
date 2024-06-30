a2ef4fa4f98461f4d259ae81f320b8d9d9d82eb0
Wan Muzaffar <wanmuz86@gmail.com>
2024-06-30 14:57:28 +0800

    setup the main.jsx file with createBrowserRouter and the routes of our web app

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,33 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Home from './components/Home.jsx'
+import Contact from './components/Contact.jsx'
+import About from './components/About.jsx'
+
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<App/>,
+    children:[
+      {
+        path:"",
+        element:<Home/>
+      },
+      {
+        path:"contact",
+        element:<Contact/>
+      },
+      {
+        path:"about",
+        element:<About/>
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+   <RouterProvider router={router}/>
 )
