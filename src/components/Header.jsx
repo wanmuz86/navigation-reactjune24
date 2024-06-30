5684fcf384737ac66287b193415ef01cfa84c533
Wan Muzaffar <wanmuz86@gmail.com>
2024-06-30 15:19:13 +0800

    add link to in header / nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
-  return (
-    <div><header>
-        <h1>Nav example</h1>
+    return (
+        <div><header>
+            <h1>Nav example</h1>
         </header>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="./">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="./about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="./contact">Contact</Link>
+                    </li>
+
+                </ul>
+            </nav>
         </div>
-  )
+    )
 }
 
 export default Header
