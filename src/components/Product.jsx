ffddbc52f428a011005a0c7d342d65a95470259c
Wan Muzaffar <wanmuz86@gmail.com>
2024-06-30 16:05:02 +0800

    search params

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
-import { useParams } from 'react-router-dom';
+import React, { useEffect } from 'react'
+import { useParams,useSearchParams } from 'react-router-dom';
 
 const Product = () => {
     let {productId} = useParams();
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    // When the page is loaded I want to retrieve the searchParams using .get method
+    // useEffect hook => To perform initialization when the component is loaded
+
+    useEffect(()=>{
+        console.log(searchParams.get('q'))
+        console.log(searchParams.get('lang'))
+
+    },
+    []);
   return (
     <div>
         <h1>Product Page</h1>
