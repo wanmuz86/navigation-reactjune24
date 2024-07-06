d1165dfaceaf1cf46d4d4273c51fbd5e151c1980
Wan Muzaffar <wanmuz86@gmail.com>
2024-07-06 09:27:22 +0800

    add protected rout

--- src/components/CreateProduct.jsx
+++ src/components/CreateProduct.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const CreateProduct = () => {
   return (
-    <div>CreateProduct</div>
+    <div>
+      <h1>Create Product</h1>
+      <p>This is a create product page, it is protected for admin only</p>
+    </div>
   )
 }
 
--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Navigate, Outlet } from 'react-router-dom'
 
 const ProtectedRoute = () => {
-  return (
-    <div>ProtectedRoute</div>
-  )
+
+    // THis is  a simple flow to imagine and authentication flow
+    // Normally in addition of checking the localstorage
+    // I need to also pass the token / call the verifytoken API
+    const localStorageToken = localStorage.getItem("token")
+    // Outlet -> the replacement of the page eg, CreatePage Component
+    return localStorageToken ? <Outlet/> : <Navigate to="/" replace />
+
 }
 
 export default ProtectedRoute
